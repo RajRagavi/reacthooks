2d9198256346debe33188c33367a56eb65c114e0
RajRagavi <143269843+RajRagavi@users.noreply.github.com>
2024-11-20 09:04:21 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import Note from "./components/Note";
+
+function App({notes}) {
+  return (
+    <div>
+      <div className="container p-5 bg-primary ">
+        <div className="row">
+          {notes.map((note) => (
+            <div className="col" key={note.id}>
+              <div className="card">
+                <div className="card-body">
+                  <Note note={note} />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+const notes = [
+  {
+    id:1,
+   title: "FREE",
+   price: 0,
+   content: {
+    text1: "Single User",
+    text2: "50GB Storage",
+    text3: "Unlimited Public Projects",
+    text4: "Community Access",
+      },
+    uncontent:{
+      text5: "Unlimtied Private Projects",
+      text6: "Deducated Phone Support",
+      text7: "Free Subdomain",
+      text8: "Monthly Status Reports",
+    },
+    important: true
+  },
+  {
+    id:2,
+    title: "PLUS",
+    price: 9,
+    content: {
+     text1: "5 User",
+     text2: "50GB Storage",
+     text3: "Unlimited Public Projects",
+     text4: "Community Access",
+     text5: "Unlimtied Private Projects",
+     text6: "Deducated Phone Support",
+     text7: "Free Subdomain",
+     
+       },
+     uncontent:{
+     text8: "Monthly Status Reports",
+     },
+     important: true
+  },
+  {
+    id:3,
+    title: "PRO",
+    price: 49,
+    content: {
+     text1: "Unlimited User",
+     text2: "50GB Storage",
+     text3: "Unlimited Public Projects",
+     text4: "Community Access",
+     text5: "Unlimtied Private Projects",
+     text6: "Deducated Phone Support",
+     text7: "Free Subdomain",
+     text8: "Monthly Status Reports",
+    
+     
+       },
+     
+       important: true
+  }
+]
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+    <App notes={notes}/>
+ 
+)
